f9fe463450be8ec206b9cf75689387597fffb5fc
criskcrash01 <crisk0047@gmail.com>
2025-11-01 05:42:23 +0000

    added intro card

--- day1_hello.py
+++ day1_hello.py
@@ -1,2 +1,14 @@
 print("hello ai world")
 print("today is the first day of learning python , though m y ai mentor")
+print("2+4=",2+4)
+print("algorithm ", "variable", "integer",sep="|")
+print("""
+      --------------------
+      STUDENT INTRO CARD
+      --------------------
+      NAME : cristin k
+      AGE : 29
+      GOAL : "Ai Developer"
+      --------------------
+      """)
+''
